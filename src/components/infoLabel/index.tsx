dea21cbbc2818b662ccf0104189b77554fafe3c4
Marino Gabel <marino.gabel@htw-berlin.de>
2025-10-02 13:45:37 +0200

    removed unused local position calculation, added outline to label

--- src/components/infoLabel/index.tsx
+++ src/components/infoLabel/index.tsx
@@ -13,33 +13,23 @@ interface InfoLabelProps {
 const InfoLabel = ({ objectRef, objectName, yPadding = 7.5, showLine = true }: InfoLabelProps
 ) => {
   const [labelPosition, setLabelPosition] = useState(new Position(0, yPadding, 0));
-  const [localLabelPosition, setLocalLabelPosition] = useState(new Position(0, yPadding, 0));
   const [top, setTop] = useState(new Position(0, 0, 0));
-
-  console.log("TOP: " + top);
-  console.log("label pos: " + labelPosition)
-  console.log("local label pos: " + localLabelPosition)
 
   useEffect(() => {
     if (!objectRef.current) return;
 
-    console.log("CALCULATE BB");
     const boundingBox = new THREE.Box3().setFromObject(objectRef.current);
     const center = boundingBox.getCenter(new THREE.Vector3());
     const size = boundingBox.getSize(new THREE.Vector3());
 
     setTop(new Position(center.x, center.y + size.y / 2, center.z));
     setLabelPosition(new Position(center.x, center.y + size.y / 2 + yPadding, center.z));
-
-    const localPos = objectRef.current.localToWorld(labelPosition.clone());
-
-    setLocalLabelPosition(new Position(localPos.x, localPos.y, localPos.z));
   }, [objectRef, yPadding]);
 
   return (
     <>
       <Billboard position={labelPosition.toArray()}>
-        <Text fontSize={1} color="black" anchorX="center" anchorY="bottom">
+        <Text fontSize={1} color="white" outlineWidth={0.05} outlineColor="black" outlineOpacity={1} anchorX="center" anchorY="bottom">
           {objectName}
         </Text>
       </Billboard>
